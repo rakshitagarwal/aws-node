03747863c480a6aea83b37476a34754eda687b4e
rakshitagarwal <rakshitag13@gmail.com>
2022-10-28 11:47:03 +0530

    scripts edited updated

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,7 +3,7 @@
 echo 'run application_start.sh: ' >> /home/ec2-user/my-app/aws-node/deploy.log
 
 echo 'stopping PM2' >> /home/ec2-user/my-app/aws-node/deploy.log
-PM2_HOME=/etc/.pm2 pm2 stop server.js --name=nodejs-express-app >> /home/ec2-user/my-app/aws-node/deploy.log 
+PM2_HOME=/etc/.pm2 pm2 stop nodejs-express-app >> /home/ec2-user/my-app/aws-node/deploy.log 
 
 echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/my-app/aws-node/deploy.log
 PM2_HOME=/etc/.pm2 pm2 start server.js --name=nodejs-express-app >> /home/ec2-user/my-app/aws-node/deploy.log 
